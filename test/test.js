a91ec1e3f2fff6a52afd3ab6d141d6bd297c7a44
Mindaugas <mindaugas.raudavicius@gmail.com>
2017-10-08 22:38:27 +0100

    Feat/basic hello with test (#1)
    * package json file.
    
    * Ignored lock file.
    
    * Added mocha and express
    
    * Added a dummy test
    
    * Travis file.

---
+++ test/test.js
@@ -0,0 +1,8 @@
+var assert = require('assert');
+describe('First test', function() {
+  describe('Dummy test', function() {
+    it('Checking if true is true', function() {
+      assert.equal(true, true);
+    });
+  });
+});
